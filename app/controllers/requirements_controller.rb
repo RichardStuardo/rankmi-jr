0c500bb24a2520d34fbc66005fa98cbdcaf5662a
Richard Stuardo <stuardorichard@gmail.com>
2019-03-23 20:40:03 -0300

    requerimientos controlador y vista

--- app/controllers/requirements_controller.rb
+++ app/controllers/requirements_controller.rb
@@ -1,4 +1,17 @@
 class RequirementsController < ApplicationController
   def index
+    @requirements = Requirement.all
+  end
+  def create
+    @requirement = Requirement.new(title: params[:requirement][:title], 
+                                  description: params[:requirement][:description]
+                                  )
+    @requirement.user = current_user
+    if @requirement.save
+      redirect_to root_path, notice: 'Requerimiento publicado'
+    else
+      redirect_to root_path, alert: 'Requerimiento no fue pulicado'
+    end
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'requirements/index'
+  resources :requirements, only: [:index, :create]
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'requirements#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
